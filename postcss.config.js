20725c35a0c5a1cf2f1e4c9b69bdc0e702b3cfed
William Martin <martinwillnorman@gmail.com>
2019-03-01 21:29:20 -0500

    added responsive-type / hexrgba

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ module.exports = {
   plugins: [
     require("autoprefixer")(),
     require("css-mqpacker")(),
+    require("postcss-hexrgba")(),
+    require("postcss-responsive-type")(),
     require("cssnano")({
       preset: [
         "default",
